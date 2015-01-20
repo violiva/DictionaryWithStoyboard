33602391c541cf8546da89aec8377c3b04abeb62
Vicente Oliva <vicente_Oliva@hotmail.com>
2015-01-20 12:26:02 +0100

    Initial Commit

---
+++ VOSDiccionarioiPadBasico/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VOSDiccionarioiPadBasico
+//
+//  Created by Vicente Oliva de la Serna on 20/1/15.
+//  Copyright (c) 2015 Vicente Oliva de la Serna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
